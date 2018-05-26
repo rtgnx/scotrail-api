a3cbf1b6f0828b362eef74b75b306274a05b45be
Adrian Cybulski <adrian@rtgnx.xyz>
2018-05-26 21:53:54 +0100

    live boards

---
+++ board.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"reflect"
+	"strconv"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const BOARD_URL = `https://www.scotrail.co.uk/nre/live-boards/`
+
+type Entry struct {
+	ID          string    `json:"id"`
+	Platform    int       `json:"platform"`
+	Destination string    `json:"destination"`
+	Departs     time.Time `json:"departs"`
+	Arrives     time.Time `json:"arrives"`
+	Expected    string    `json:"expected"`
+	Origin      string    `json:"origin"`
+	Operator    string    `json:"operator"`
+}
+
+type Board []Entry
+
+func getBoard(station string) Board {
+	res, err := http.Get(BOARD_URL + station)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	if res.StatusCode != 200 {
+		log.Fatalln(res.Status)
+	}
+
+	defer res.Body.Close()
+
+	return ParseBoard(res.Body)
+}
+
+func ParseBoard(r io.Reader) (b Board) {
+	doc, _ := goquery.NewDocumentFromReader(r)
+
+	doc.Find("tr.service").Each(func(i int, s *goquery.Selection) {
+		var e Entry
+
+		if id, ok := s.Attr("data-id"); ok {
+			e.ID = id
+		}
+
+		s.ChildrenFiltered("td").Each(func(i int, s *goquery.Selection) {
+			switch s.AttrOr("data-label", "") {
+			case "Expected", "Origin", "Destination", "Operator":
+				label := s.AttrOr("data-label", "")
+				reflect.ValueOf(&e).Elem().FieldByName(label).SetString(s.Text())
+			case "Platform":
+				e.Platform = platform(s.Text())
+			case "Arrives", "Departs":
+				t := time.Now()
+				t.Format(s.Text())
+				label := s.AttrOr("data-label", "")
+				reflect.ValueOf(&e).Elem().FieldByName(label).Set(reflect.ValueOf(t))
+			}
+		})
+
+		b = append(b, e)
+	})
+	return
+}
+
+func platform(p string) int {
+	i, _ := strconv.ParseInt(p, 10, 32)
+	return int(i)
+}
---
+++ board_test.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+const TEST_BOARD = `
+	<div id="nre-live-boards" data-crs="KNS">
+		<h4>Departure board</h4>
+			<table class="table responsive">
+				<thead>
+					<tr>
+						<th>Platform</th>
+						<th>Destination</th>
+						<th>Departs</th>
+						<th>Expected</th>
+						<th>Origin</th>
+						<th>Operator</th>
+					</tr>
+				</thead>
+				<tbody>
+									<tr class="service" data-id="8SlZWSDid2CaZh1W0UJYWA%3D%3D">
+						<td data-label="Platform">1</td>
+						<td data-label="Destination">Glasgow Central</td>
+						<td data-label="Departs">19:34</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Origin">Barrhead</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="F8AJ%23Z7tYsWG0lSJcEWzRg%3D%3D">
+						<td data-label="Platform">2</td>
+						<td data-label="Destination">Kilmarnock</td>
+						<td data-label="Departs">19:45</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Origin">Glasgow Central</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="%23wAabExJDdHHupQ%2BUU1JgA%3D%3D">
+						<td data-label="Platform">1</td>
+						<td data-label="Destination">Glasgow Central</td>
+						<td data-label="Departs">19:59</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Origin">Kilmarnock</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="hE2LKFuzXSUTiq9caPpEXA%3D%3D">
+						<td data-label="Platform">-</td>
+						<td data-label="Destination">Kilmarnock</td>
+						<td data-label="Departs">20:45</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Origin">Glasgow Central</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="kiGlAM4dMS9jLfKuIT4TIQ%3D%3D">
+						<td data-label="Platform">-</td>
+						<td data-label="Destination">Glasgow Central</td>
+						<td data-label="Departs">20:59</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Origin">Kilmarnock</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+								</tbody>
+			</table>
+		<h4>Arrival board</h4>
+			<table class="table responsive">
+				<thead>
+					<tr>
+						<th>Platform</th>
+						<th>Origin</th>
+						<th>Arrives</th>
+						<th>Expected</th>
+						<th>Destination</th>
+						<th>Operator</th>
+					</tr>
+				</thead>
+				<tbody>
+									<tr class="service" data-id="8SlZWSDid2CaZh1W0UJYWA%3D%3D">
+						<td data-label="Platform">1</td>
+						<td data-label="Origin">Barrhead</td>
+						<td data-label="Arrives">19:34</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Destination">Glasgow Central</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="F8AJ%23Z7tYsWG0lSJcEWzRg%3D%3D">
+						<td data-label="Platform">2</td>
+						<td data-label="Origin">Glasgow Central</td>
+						<td data-label="Arrives">19:45</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Destination">Kilmarnock</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="%23wAabExJDdHHupQ%2BUU1JgA%3D%3D">
+						<td data-label="Platform">1</td>
+						<td data-label="Origin">Kilmarnock</td>
+						<td data-label="Arrives">19:59</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Destination">Glasgow Central</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="hE2LKFuzXSUTiq9caPpEXA%3D%3D">
+						<td data-label="Platform">-</td>
+						<td data-label="Origin">Glasgow Central</td>
+						<td data-label="Arrives">20:45</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Destination">Kilmarnock</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+									<tr class="service" data-id="kiGlAM4dMS9jLfKuIT4TIQ%3D%3D">
+						<td data-label="Platform">-</td>
+						<td data-label="Origin">Kilmarnock</td>
+						<td data-label="Arrives">20:59</td>
+						<td data-label="Expected">On time</td>
+						<td data-label="Destination">Glasgow Central</td>
+						<td data-label="Operator">ScotRail</td>
+					</tr>
+								</tbody>
+			</table>
+	</div>
+`
+
+func TestParseBoard(t *testing.T) {
+	board := ParseBoard(strings.NewReader(TEST_BOARD))
+	if len(board) < 1 {
+		t.Errorf("No data was extracted")
+	}
+}
