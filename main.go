9d59533865bd1df9d81f86ebc63425ee6f6393f7
Adrian Cybulski <adrian@rtgnx.xyz>
2018-05-26 22:31:34 +0100

    Service details http endpoint

--- main.go
+++ main.go
@@ -19,6 +19,18 @@ func GetLiveBoard(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, b)
 }
 
+func GetServiceDetails(ctx echo.Context) error {
+	id := ctx.Param("id")
+
+	service := serviceDetails(id)
+
+	if len(service) < 1 {
+		ctx.String(http.StatusNotAcceptable, "Invalid service id")
+	}
+
+	return ctx.JSON(http.StatusOK, service)
+}
+
 func Hello(ctx echo.Context) error {
 	return ctx.String(http.StatusOK, "Hello, World")
 }
@@ -29,6 +41,7 @@ func main() {
 
 	e.GET("/", Hello)
 	e.GET("/live/:stn", GetLiveBoard)
+	e.GET("/service/:id", GetServiceDetails)
 
 	e.Start(addr)
 }
