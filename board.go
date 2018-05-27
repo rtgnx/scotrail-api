935e41ef8287dbf0fd72030d9576732b353b9576
Adrian Cybulski <adrian@rtgnx.xyz>
2018-05-27 11:01:32 +0100

    Solves duplicate stations problem
    Because there are 2 html tables 1 with arrival times
    and one with departures, ParseBoard() should check if specific service
    was already added to Board and only update times

--- board.go
+++ board.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"reflect"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
@@ -26,6 +27,16 @@ type Entry struct {
 
 type Board []Entry
 
+func (b *Board) HasStation(id string) (bool, int) {
+	for i, v := range *b {
+		if strings.Compare(v.ID, id) == 0 {
+			return true, i
+		}
+	}
+
+	return false, 0
+}
+
 func getBoard(station string) Board {
 	res, err := http.Get(BOARD_URL + station)
 	if err != nil {
@@ -45,27 +56,34 @@ func ParseBoard(r io.Reader) (b Board) {
 	doc, _ := goquery.NewDocumentFromReader(r)
 
 	doc.Find("tr.service").Each(func(i int, s *goquery.Selection) {
-		var e Entry
+		var e *Entry = new(Entry)
 
-		if id, ok := s.Attr("data-id"); ok {
-			e.ID = id
+		id, ok := s.Attr("data-id")
+
+		if ok {
+			if ok, idx := b.HasStation(id); ok {
+				e = &(b)[idx]
+			}
 		}
 
 		s.ChildrenFiltered("td").Each(func(i int, s *goquery.Selection) {
 			switch s.AttrOr("data-label", "") {
 			case "Expected", "Origin", "Destination", "Operator":
 				label := s.AttrOr("data-label", "")
-				reflect.ValueOf(&e).Elem().FieldByName(label).SetString(s.Text())
+				reflect.ValueOf(e).Elem().FieldByName(label).SetString(s.Text())
 			case "Platform":
 				e.Platform = platform(s.Text())
 			case "Arrives", "Departs":
 				t := ParseTime(s.Text())
 				label := s.AttrOr("data-label", "")
-				reflect.ValueOf(&e).Elem().FieldByName(label).Set(reflect.ValueOf(t))
+				reflect.ValueOf(e).Elem().FieldByName(label).Set(reflect.ValueOf(t))
 			}
 		})
 
-		b = append(b, e)
+		if len(e.ID) < 1 {
+			e.ID = id
+			b = append(b, *e)
+		}
 	})
 	return
 }
