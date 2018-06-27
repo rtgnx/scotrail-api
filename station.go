30a18700a3d32bd951988d47390b42398b014a3f
Adrian Cybulski <adrian@rtgnx.xyz>
2018-06-27 13:19:25 +0100

    Distance between 2 cordinates

--- station.go
+++ station.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"math"
 	"regexp"
 )
 
@@ -16,6 +17,11 @@ type Station struct {
 type StationList map[string]Station
 
 var Stations StationList = make(map[string]Station)
+
+// Return map distance(meters) => Station
+func (s *StationList) Nearest() (st map[int]Station) {
+	return
+}
 
 func (s *StationList) Search(name string, limit int) (l []Station) {
 	for _, v := range *s {
@@ -50,4 +56,30 @@ func load_stations(path string) {
 		Stations[s.Code] = s
 	}
 
+}
+
+type Cordinate struct {
+	long, lat float64
+}
+
+// Conversion from degress to radians
+func deg2rad(x float64) float64 {
+	return x * float64((math.Pi / 180))
+}
+
+// Returns distance between two cordinates
+func distance(a, b Cordinate) int {
+	var R = 6371e3 // metres
+	var φ1 = deg2rad(a.lat)
+	var φ2 = deg2rad(b.lat)
+	var Δφ = deg2rad(b.lat - a.lat)
+	var Δλ = deg2rad(b.long - a.long)
+
+	var an = math.Sin(Δφ/2)*math.Sin(Δφ/2) + math.Cos(φ1)*math.Cos(φ2)*math.Sin(Δλ/2)*math.Sin(Δλ/2)
+
+	var c = 2 * math.Atan2(math.Sqrt(an), math.Sqrt(1-an))
+
+	var d = R * c
+	// Convert to meters and cast to int
+	return int(d / 1000)
 }
